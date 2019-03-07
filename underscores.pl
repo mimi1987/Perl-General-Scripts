80f6573687789088781d6ecb7c09d8f195e512c6
Michael M <m.migsch@gmail.com>
2019-03-07 14:13:05 +0100

    Update underscores.pl

--- underscores.pl
+++ underscores.pl
@@ -2,6 +2,7 @@ use strict;
 use warnings;
 use v5.14;
 
+# prefix and suffix a number with underscores and if the last character is a number the suffic underscore is removed.
 my $text = <STDIN>;
 $text =~ s/(\d)+/_$1_/ig;
 print lc substr $text, 0, -1;
