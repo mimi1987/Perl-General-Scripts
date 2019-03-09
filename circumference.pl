c4630412b591898fe56bab12a35bfa4ef6cda1f3
Michael M <m.migsch@gmail.com>
2019-03-09 22:03:47 +0100

    Update circumference.pl

--- circumference.pl
+++ circumference.pl
@@ -3,7 +3,13 @@ use v5.8;
 use warnings;
 use constant PI => 3.141592654;
 
+print "Please enter a radius: ";
 my $radius = <STDIN>;
 chomp $radius;
 my $circumference = $radius * 2 * PI;
-print "$circumference\n";
+if ($circumference <= 0){
+  print "The circumference of the circle is 0.";
+}
+else {
+    print "The circumference of the circle is $circumference\n";
+}
