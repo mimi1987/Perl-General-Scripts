16a4d04e15172fbcd36b84fd5257ff902bfe7f71
Michael M <m.migsch@gmail.com>
2019-06-07 16:32:41 +0200

    Create above_average.pl

---
+++ above_average.pl
@@ -0,0 +1,25 @@
+sub average {
+  my $number_of_elements = 0;
+  foreach (@_) {
+    $number_of_elements += 1;
+  }
+  $number_of_elements;
+}
+
+sub above_average {
+  my $divident = &average(@_);
+  my $average = 0;
+  my @result = ();
+  foreach (@_) {
+    $average += $_;
+    push @result, $_;
+  }
+  my $quotient = $average / $divident;
+  foreach (@result) {
+    if ($_ > $quotient) {
+      print "I'm above average: $_.\n";
+    }
+  }
+  }
+
+&above_average(100, 1..10);
