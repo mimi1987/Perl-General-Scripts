161c3fe5d53e79e3392c32b5aa3e40155eb34aeb
Michael M <m.migsch@gmail.com>
2019-06-14 17:07:58 +0200

    Update set_of_exercises.pl

--- set_of_exercises.pl
+++ set_of_exercises.pl
@@ -14,3 +14,24 @@ my %names = (
 print "Who do you want? Enter a name:\n";
 chomp(my $name = <STDIN>);
 print "The family name of $name is $names{$name}.\n";
+
+
+# EXERCISE 2,CHAPTER 6 HASHES - Count the appearence of words.
+# Read a series of words (one word per line) until end of input.
+my(@words, $word, %word_seen_before);
+print "Please enter some words on separate lines: \n";
+@words = <STDIN>;
+
+
+# Print a summary of how many times a word was seen.
+ foreach $word (@words) {
+   if (exists $word_seen_before{$word}) {
+     $word_seen_before{$word} += 1;
+   } else {
+     $word_seen_before{$word} = 1;
+   }
+ }
+
+ while (my($key, $value) = each %word_seen_before) {
+   print "$key => $value\n";
+ }
