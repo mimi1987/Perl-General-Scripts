c9f1843789efa436e58c56e48e377e4516ccc63a
Michael M <m.migsch@gmail.com>
2019-08-27 12:08:46 +0200

    Update zoll_to_cm.pl

--- zoll_to_cm.pl
+++ zoll_to_cm.pl
@@ -1,10 +1,12 @@
 use strict;
+use POSIX;
 
 # Converts a passed zoll value to cm and prints the result in the needed format
 sub zoll_to_cm
 {
   my $zoll = shift @_;
-  print "$zoll\"/" . $zoll * 2.54 . "cm" . "\n";
+  print "$zoll\"/" . floor($zoll * 2.54) . "cm" . "\n";
 }
 
-&zoll_to_cm(100);
+&zoll_to_cm(134);
+
